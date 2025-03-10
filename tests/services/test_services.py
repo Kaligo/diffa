fcd1ad3bfbc2d16ec94d0e7e4a90b95627d49798
Quoc Nguyen <quoc.nguyen@ascenda.com>
2025-03-10 11:27:42 +0700

    Unit Tests for the services modules

---
+++ tests/services/test_services.py
@@ -0,0 +1,143 @@
+from datetime import datetime
+
+import pytest
+
+from diffa.services import DiffaService
+from diffa.db.data_models import CountCheck, MergedCountCheck
+
+
+@pytest.fixture
+def diffa_service():
+    return DiffaService()
+
+
+@pytest.mark.parametrize(
+    "source_counts, target_counts, expected_merged_counts",
+    [
+        # case 1: Checking dates are in both source and target
+        (
+            [
+                CountCheck(
+                    cnt=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ],
+            [
+                CountCheck(
+                    cnt=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ],
+            [
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date()
+                )
+            ]
+        ),
+        # case 2: Checking dates are in source only
+        (
+            [
+                CountCheck(
+                    cnt=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ],
+            [],
+            [
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=0,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ]
+        ),
+        # case 3: Checking dates are in target only
+        (
+            [],
+            [
+                CountCheck(
+                    cnt=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ],
+            [
+                MergedCountCheck(
+                    source_count=0,
+                    target_count=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                )
+            ]
+        ),
+        # case 4: Checking dates are in neither source nor target
+        (
+            [],
+            [],
+            []
+        ),
+    ],
+)
+def test__merge_count_check(
+    diffa_service, source_counts, target_counts, expected_merged_counts
+):
+    merged_counts = diffa_service._merge_count_checks(source_counts, target_counts)
+    assert expected_merged_counts == merged_counts
+
+
+@pytest.mark.parametrize(
+    "merged_count_checks, expected_result",
+    [
+        # case 1: All merged count checks are valid
+        [
+            [
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                ),
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=150,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                ),
+            ],
+            False,
+        ],
+        # case 2: All merged count checks are invalid
+        [
+            [
+                MergedCountCheck(
+                    source_count=150,
+                    target_count=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                ),
+            ],
+            True,
+        ],
+        # case 3: Mixed valid and invalid merged count checks
+        [
+            [
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                ),
+                MergedCountCheck(
+                    source_count=150,
+                    target_count=100,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                ),
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=150,
+                    check_date=datetime.strptime("2024-01-03", "%Y-%m-%d").date(),
+                ),
+            ],
+            True,
+        ],
+    ],
+)
+def test__check_if_invalid_diff(diffa_service, merged_count_checks, expected_result):
+    is_invalid_diff = diffa_service._check_if_invalid_diff(merged_count_checks)
+    assert is_invalid_diff == expected_result
