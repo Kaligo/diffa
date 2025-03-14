a1afa85e19de6cb8eca971eb32a62953840041e9
Quoc Nguyen <quoc.nguyen@ascenda.com>
2025-03-14 16:20:44 +0700

    update the unit tests

--- src/diffa/config.py
+++ src/diffa/config.py
@@ -2,6 +2,7 @@ import os
 import json
 from datetime import date
 from enum import Enum
+from abc import ABC, abstractmethod
 
 import dsnparse
 
@@ -11,6 +12,7 @@ CONFIG_DIR = os.path.expanduser("~/.diffa")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
 DIFFA_DB_SCHEMA = "diffa"
 DIFFA_DB_TABLE = "diffa_checks"
+DIFFA_CHECK_RUNS_TABLE = "diffa_check_runs"
 DIFFA_BEGIN_DATE = date(2024, 1, 1)
 
 
@@ -21,41 +23,146 @@ class ExitCode(Enum):
 logger = Logger(__name__)
 
 
+class BaseConfig(ABC):
+    def __init__(self, db_info: str = None, database: str = None):
+        self.db_info = db_info
+        self.database = database
+        self.parsed_db_info = None
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, self.__class__):
+            return NotImplemented
+        return self.__dict__ == __value.__dict__
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__dict__})"
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key) and value is not None:
+                setattr(self, key, value)
+
+    def parse_db_info(self):
+        if not self.parsed_db_info:
+            try:
+                dns = dsnparse.parse(self.db_info)
+                self.parsed_db_info = self._extract_db_details(dns)
+                self._validate_parsed_db_info(self.parsed_db_info)
+            except TypeError:
+                logger.error("Invalid db info", exc_info=True)
+                raise
+        return self.parsed_db_info
+
+    def _validate_parsed_db_info(self, parsed_db_info: dict):
+        for key, value in parsed_db_info.items():
+            if not value:
+                raise ValueError(
+                    f"Configuration for {key} is missing. Please provide it!"
+                )
+
+    @abstractmethod
+    def _extract_db_details(self, dns) -> dict:
+        pass
+
+    def get_db_config(self):
+        return self.parse_db_info()
+
+    def get_database(self):
+        return self.get_db_config().get("database")
+
+    def get_schema(self):
+        return self.get_db_config().get("schema")
+
+    def get_db_info(self):
+        return self.db_info
+
+    def get_table(self):
+        return self.get_db_config().get("table")
+
+
+class SourceTargetConfig(BaseConfig):
+    def __init__(
+        self,
+        db_info: str = None,
+        database: str = None,
+        schema: str = None,
+        table: str = None,
+    ):
+        super().__init__(db_info, database)
+        self.schema = schema
+        self.table = table
+
+    def _extract_db_details(self, dns):
+        db_database = self.database or dns.database
+        return {
+            "host": dns.host,
+            "scheme": dns.scheme,
+            "port": dns.port,
+            "database": db_database,
+            "user": dns.username,
+            "password": dns.password,
+            "schema": self.schema,
+            "table": self.table,
+            "db_url": f"{dns.scheme}://{dns.username}:{dns.password}@{dns.host}:{dns.port}/{db_database}",
+        }
+
+
+class DiffaDBConfig(BaseConfig):
+    def __init__(self, db_info: str = None, database: str = None):
+        super().__init__(db_info, database)
+        self.schema = DIFFA_DB_SCHEMA
+        self.tables = {
+            "checks": DIFFA_DB_TABLE,
+            "check_runs": DIFFA_CHECK_RUNS_TABLE,
+        }
+
+    def _extract_db_details(self, dns):
+        db_database = self.database or dns.database
+        return {
+            "host": dns.host,
+            "scheme": dns.scheme,
+            "port": dns.port,
+            "database": db_database,
+            "user": dns.username,
+            "password": dns.password,
+            "schema": self.schema,
+            "tables": self.tables,
+            "db_url": f"{dns.scheme}://{dns.username}:{dns.password}@{dns.host}:{dns.port}/{db_database}",
+        }
+
+    def parse_db_info(self, table_key: str = None):
+        if not self.parsed_db_info:
+            try:
+                dns = dsnparse.parse(self.db_info)
+                self.parsed_db_info = self._extract_db_details(dns)
+            except TypeError:
+                logger.error("Invalid db info", exc_info=True)
+                raise
+        self.parsed_db_info["table"] = self.tables[table_key] if table_key else None
+        return self.parsed_db_info
+
+    def get_db_config(self, table_key: str = None):
+        return self.parse_db_info(table_key)
+
+    def get_table(self, table_key: str):
+        return self.get_db_config(table_key)["table"]
+
+
 class ConfigManager:
-    """Singleton Pattern for ConfigManager to ensure that the config is loaded only once"""
-
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+    """Manage all the configuration needed for Diffa Operations"""
 
     def __init__(
         self,
+        source_config: SourceTargetConfig = SourceTargetConfig(),
+        target_config: SourceTargetConfig = SourceTargetConfig(),
+        diffa_config: DiffaDBConfig = DiffaDBConfig(),
     ):
-        if not hasattr(self, "config"):
-            self.config = {
-                "source": {
-                    "db_info": None,
-                    "database": None,
-                    "schema": None,
-                    "table": None,
-                },
-                "target": {
-                    "db_info": None,
-                    "database": None,
-                    "schema": None,
-                    "table": None,
-                },
-                "diffa": {
-                    "db_info": None,
-                    "database": None,
-                    "schema": DIFFA_DB_SCHEMA,
-                    "table": DIFFA_DB_TABLE,
-                },
-            }
-            self.__load_config()
+        self.config = {
+            "source": source_config,
+            "target": target_config,
+            "diffa": diffa_config,
+        }
+        self.__load_config()
 
     def configure(
         self,
@@ -71,25 +178,19 @@ class ConfigManager:
         diffa_db_info: str = None,
     ):
         self.config["source"].update(
-            {
-                "db_info": source_db_info or self.config["source"].get("db_info"),
-                "database": source_database or self.config["source"].get("database"),
-                "schema": source_schema or self.config["source"].get("schema"),
-                "table": source_table,
-            }
+            db_info=source_db_info,
+            database=source_database,
+            schema=source_schema,
+            table=source_table,
         )
         self.config["target"].update(
-            {
-                "db_info": target_db_info or self.config["target"].get("db_info"),
-                "database": target_database or self.config["target"].get("database"),
-                "schema": target_schema or self.config["target"].get("schema"),
-                "table": target_table,
-            }
+            db_info=target_db_info,
+            database=target_database,
+            schema=target_schema,
+            table=target_table,
         )
         self.config["diffa"].update(
-            {
-                "db_info": diffa_db_info or self.config["diffa"].get("db_info"),
-            }
+            db_info=diffa_db_info,
         )
 
     def __load_config(self):
@@ -100,53 +201,32 @@ class ConfigManager:
                 uri_config = json.load(f)
 
         self.config["source"].update(
-            {"db_info": os.getenv("DIFFA__SOURCE_URI", uri_config.get("source_uri"))}
+            db_info=self.config["source"].db_info
+            or os.getenv("DIFFA__SOURCE_URI", uri_config.get("source_uri"))
         )
         self.config["target"].update(
-            {"db_info": os.getenv("DIFFA__TARGET_URI", uri_config.get("target_uri"))}
+            db_info=self.config["target"].db_info
+            or os.getenv("DIFFA__TARGET_URI", uri_config.get("target_uri"))
         )
         self.config["diffa"].update(
-            {
-                "db_info": os.getenv(
-                    "DIFFA__DIFFA_DB_URI", uri_config.get("diffa_uri")
-                ),
-            }
+            db_info=self.config["diffa"].db_info
+            or os.getenv("DIFFA__DIFFA_DB_URI", uri_config.get("diffa_uri")),
         )
 
-    def __parse_db_config(self, db_key: str):
-        try:
-            dns = dsnparse.parse(self.config[db_key]["db_info"])
-            db_database = self.config[db_key]["database"] or dns.database
-            db_schema = self.config[db_key]["schema"]
-            db_table = self.config[db_key]["table"]
-        except TypeError:
-            logger.error(
-                f"There's something wrong with the db info for {db_key}", exc_info=True
-            )
-            raise
-        return {
-            "host": dns.host,
-            "scheme": dns.scheme,
-            "port": dns.port,
-            "database": db_database,
-            "user": dns.username,
-            "password": dns.password,
-            "schema": db_schema,
-            "table": db_table,
-            "db_url": f"{dns.scheme}://{dns.username}:{dns.password}@{dns.host}:{dns.port}/{db_database}",
-        }
+    def get_config(self, db_key: str):
+        return self.config[db_key]
 
-    def get_db_config(self, db_key: str):
-        return self.__parse_db_config(db_key=db_key)
+    def get_db_config(self, db_key: str, *args, **kwargs):
+        return self.config[db_key].get_db_config(*args, **kwargs)
 
-    def get_database(self, db_key: str):
-        return self.__parse_db_config(db_key)["database"]
+    def get_database(self, db_key: str, *args, **kwargs):
+        return self.config[db_key].get_database(*args, **kwargs)
 
-    def get_schema(self, db_key: str):
-        return self.__parse_db_config(db_key)["schema"]
+    def get_schema(self, db_key: str, *args, **kwargs):
+        return self.config[db_key].get_schema(*args, **kwargs)
 
-    def get_table(self, db_key: str):
-        return self.__parse_db_config(db_key)["table"]
+    def get_table(self, db_key: str, *args, **kwargs):
+        return self.config[db_key].get_table(*args, **kwargs)
 
-    def get_db_info(self, db_key: str):
-        return self.config[db_key]["db_info"]
+    def get_db_info(self, db_key: str, *args, **kwargs):
+        return self.config[db_key].get_db_info(*args, **kwargs)
