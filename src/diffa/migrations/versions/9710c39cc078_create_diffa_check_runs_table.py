a1cce317b08a67fcdb5ca22fefec071ad2b47875
Quoc Nguyen <quoc.nguyen@ascenda.com>
2025-03-14 16:36:06 +0700

    All the codebase for the full context

---
+++ src/diffa/migrations/versions/9710c39cc078_create_diffa_check_runs_table.py
@@ -0,0 +1,72 @@
+"""create diffa_check_runs table
+
+Revision ID: 9710c39cc078
+Revises: 2f7730d11b26
+Create Date: 2025-03-11 00:46:18.178930
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+from diffa.config import ConfigManager
+
+
+# revision identifiers, used by Alembic.
+revision: str = "9710c39cc078"
+down_revision: Union[str, None] = "2f7730d11b26"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+config_manager = ConfigManager()
+
+
+def upgrade() -> None:
+    op.create_table(
+        f"{config_manager.get_table('diffa', 'check_runs')}",
+        sa.Column("run_id", sa.UUID, primary_key=True),
+        sa.Column("source_database", sa.String, nullable=False),
+        sa.Column("source_schema", sa.String, nullable=False),
+        sa.Column("source_table", sa.String, nullable=False),
+        sa.Column("target_database", sa.String, nullable=False),
+        sa.Column("target_schema", sa.String, nullable=False),
+        sa.Column("target_table", sa.String, nullable=False),
+        sa.Column("status", sa.String, nullable=False),
+        sa.Column(
+            "created_at", sa.DateTime, server_default=sa.func.now(), nullable=False
+        ),
+        sa.Column(
+            "updated_at", sa.DateTime, server_default=sa.func.now(), nullable=False
+        ),
+        schema=config_manager.get_schema("diffa"),
+    )
+    op.create_index(
+        "idx_unique_running_check_runs",
+        table_name=f"{config_manager.get_table('diffa', 'check_runs')}",
+        columns=[
+            "source_database",
+            "source_schema",
+            "source_table",
+            "target_database",
+            "target_schema",
+            "target_table",
+        ],
+        unique=True,
+        schema=config_manager.get_schema("diffa"),
+        postgresql_where=sa.text("status = 'RUNNING'"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(
+        "idx_unique_running_check_runs",
+        table_name=f"{config_manager.get_table('diffa', 'check_runs')}",
+        schema=config_manager.get_schema("diffa"),
+        if_exists=True,
+    )
+    op.drop_table(
+        f"{config_manager.get_table('diffa', 'check_runs')}",
+        schema=config_manager.get_schema("diffa"),
+    )
