378c13510b6af20e5f2ca6054609298ba6781869
Quoc Nguyen <quoc.nguyen@ascenda.com>
2025-11-07 15:08:59 +0700

    adding unit tests

--- tests/managers/test_check_manager.py
+++ tests/managers/test_check_manager.py
@@ -238,3 +238,119 @@ def test__merge_count_check(
 def test__merge_count_check_with_dimensions(check_manager, source_counts, target_counts, expected_merged_counts):
     merged_counts = check_manager._merge_count_checks(source_counts, target_counts)
     assert expected_merged_counts == merged_counts
+
+
+@pytest.mark.parametrize(
+    "merged_count_checks, expected_merged_by_date",
+    [
+        # Case 1: Merge count checks by check date with base dimension field
+        (
+            [
+                MergedCountCheck(
+                    source_count=100,
+                    target_count=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date()
+                ),
+                MergedCountCheck(
+                    source_count=300,
+                    target_count=400,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date()
+                ),
+            ],
+            {
+                datetime.strptime("2024-01-01", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=100,
+                    target_count=200,
+                    is_valid=True,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date()
+                ),
+                datetime.strptime("2024-01-02", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=300,
+                    target_count=400,
+                    is_valid=True,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date()
+                ),
+            }
+        ),
+        # Case 2: Merge count checks by check date with 1 dimension field (happy case)
+        (
+            [
+                MergedCountCheck.create_with_dimensions(["status"])(
+                    source_count=100,
+                    target_count=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                    status="True"
+                ),
+                MergedCountCheck.create_with_dimensions(["status"])(
+                    source_count=200,
+                    target_count=300,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                    status="False"
+                ),
+                MergedCountCheck.create_with_dimensions(["status"])(
+                    source_count=400,
+                    target_count=300,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                    status="True"
+                ),
+            ],
+            {
+                datetime.strptime("2024-01-01", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=300,
+                    target_count=500,
+                    is_valid=True,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                ),
+                datetime.strptime("2024-01-02", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=400,
+                    target_count=300,
+                    is_valid=False,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                ),
+            }
+        ),
+        # Case 3: Merge count checks by check date with 2 dimension fields (unhappy case: dimenssion failure => invalid diff)
+        (
+            [
+                MergedCountCheck.create_with_dimensions(["status", "country"])(
+                    source_count=100,
+                    target_count=200,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                    status="True",
+                    country="US"
+                ),
+                MergedCountCheck.create_with_dimensions(["status", "country"])(
+                    source_count=200,
+                    target_count=100,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                    status="False",
+                    country="US"
+                ),
+                MergedCountCheck.create_with_dimensions(["status", "country"])(
+                    source_count=300,
+                    target_count=400,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                    status="True",
+                    country="Singapore"
+                ),
+            ],
+            {
+                datetime.strptime("2024-01-01", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=300,
+                    target_count=300,
+                    is_valid=False,
+                    check_date=datetime.strptime("2024-01-01", "%Y-%m-%d").date(),
+                ),
+                datetime.strptime("2024-01-02", "%Y-%m-%d").date(): MergedCountCheck(
+                    source_count=300,
+                    target_count=400,
+                    is_valid=True,
+                    check_date=datetime.strptime("2024-01-02", "%Y-%m-%d").date(),
+                ),
+            }
+        ),
+    ]
+)
+def test__merge_by_check_date(check_manager, merged_count_checks, expected_merged_by_date):
+    merged_by_date = check_manager._merge_by_check_date(merged_count_checks)
+    assert expected_merged_by_date == merged_by_date
